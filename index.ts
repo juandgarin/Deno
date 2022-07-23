47da850e98832c90cc6bebc34eaf7f7b6534414d
Juan Diego Garin <juan.garin@hotelverse.tech>
2022-07-23 20:32:42 +0200

    OaK Añadido

--- index.ts
+++ index.ts
@@ -1,25 +1,9 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
+import { Application } from "https://deno.land/x/oak/mod.ts";
 
-import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
+const app = new Application();
 
-console.log("Hello from Functions!")
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
 
-serve((_req) => {
-    /*   const { name } = await req.json() */
-    const data = {
-        message: `Hello - so fast !!!`,
-    }
-
-    return new Response(
-        JSON.stringify(data),
-        { headers: { "Content-Type": "application/json" } },
-    )
-})
-
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
+await app.listen({ port: 8000 });
